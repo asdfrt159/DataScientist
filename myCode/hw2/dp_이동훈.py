3ac3fcd202f9792c18abf4ac63031aad55a8fb99
asdfrt159m <asdfrt159@naver.com>
2024-03-08 19:02:24 +0900

    '0308'

--- myCode/hw2/dp_이동훈.py
+++ myCode/hw2/dp_이동훈.py
@@ -1,42 +1,39 @@
-import numpy as np
-
-
 def algo(df, al_df, num, col) :
     if col == 0 : 
         if num == 1 :
-            val = df[0,col] - df[2,col]
+            val = df[0][col] - df[2][col]
         elif num == 2 :
-            val = df[0,col] - df[1,col]
+            val = df[0][col] - df[1][col]
         elif num == 3 :
-            val = df[1,col] - df[2,col]
+            val = df[1][col] - df[2][col]
         elif num == 4 : 
-            val = df[2,col] - df[1,col]
+            val = df[2][col] - df[1][col]
         elif num == 5 :
-            val = df[1,col] - df[0,col]
+            val = df[1][col] - df[0][col]
         elif num == 6 : 
-            val = df[2,col] - df[0,col]
+            val = df[2][col] - df[0][col]
         else : 
             val = None
         return val
     else : 
         if num == 1 :
-            val = df[0,col] - df[2,col]
-            val = val + max(al_df[4-1,col-1],al_df[5-1,col-1])
+            val = df[0][col] - df[2][col]
+            val = val + max(al_df[4-1][col-1],al_df[5-1][col-1])
         elif num == 2 :
-            val = df[0,col] - df[1,col]
-            val = val + max(al_df[3-1,col-1],al_df[6-1,col-1])
+            val = df[0][col] - df[1][col]
+            val = val + max(al_df[3-1][col-1],al_df[6-1][col-1])
         elif num == 3 :
-            val = df[1,col] - df[2,col]
-            val = val + max(al_df[2-1,col-1],al_df[6-1,col-1])
+            val = df[1][col] - df[2][col]
+            val = val + max(al_df[2-1][col-1],al_df[6-1][col-1])
         elif num == 4 : 
-            val = df[2,col] - df[1,col]
-            val = val + max(al_df[1-1,col-1],al_df[5-1,col-1])
+            val = df[2][col] - df[1][col]
+            val = val + max(al_df[1-1][col-1],al_df[5-1][col-1])
         elif num == 5 :
-            val = df[1,col] - df[0,col]
-            val = val + max(al_df[1-1,col-1],al_df[4-1,col-1])
+            val = df[1][col] - df[0][col]
+            val = val + max(al_df[1-1][col-1],al_df[4-1][col-1])
         elif num == 6 : 
-            val = df[2,col] - df[0,col]
-            val = val + max(al_df[2-1,col-1],al_df[3-1,col-1])
+            val = df[2][col] - df[0][col]
+            val = val + max(al_df[2-1][col-1],al_df[3-1][col-1])
         else : 
             val = None
         return val
@@ -52,24 +49,25 @@ def main() :
     for i in range(len(lines)) :
         if i%4 != 1 :
             continue
-        arr_list.append(np.array([[int(x) for x in lines[i].split(' ')],[int(x) for x in lines[i+1].split(' ')],[int(x) for x in lines[i+2].split(' ')]]))
+        arr_list.append([[int(x) for x in lines[i].split(' ')],[int(x) for x in lines[i+1].split(' ')],[int(x) for x in lines[i+2].split(' ')]])
 
     ans = []
     # 배열 생성
     for k in range(len(arr_list)) :
         max_list = []
-        arr = np.empty((6,arr_list[k].shape[1]))
-        arr[:] = np.nan
+        arr = [[None for _ in range(len(arr_list[k][0]))] for _ in range(6)]
 
-        for j in range(arr_list[k].shape[1]) :  # 0 ~ 9
+
+        for j in range(len(arr_list[k][0])) :  # 0 ~ 9
             for i in range(6) :  # 0 ~ 5
-                arr[i, j] = algo(arr_list[k],arr,i+1, j)
+                arr[i][j] = algo(arr_list[k],arr,i+1,j)
         
         for i in range(6) :
-            max_list.append(arr[i,arr_list[k].shape[1]-1])
+            max_list.append(arr[i][len(arr_list[k][0])-1])
 
         ans.append(int(max(max_list)))
 
+    # print(ans)
     with open('output4.txt','w') as f:
         for i in range(len(ans)) :
             f.write('#'+ str(i+1) + ' ' +str(ans[i]) + '\n')
