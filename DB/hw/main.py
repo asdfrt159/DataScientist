54417c6d1ba0a548da9e54009f8228acc6321731
asdfrt159m <asdfrt159@naver.com>
2024-03-27 20:38:22 +0900

    '0327db'

--- DB/hw/main.py
+++ DB/hw/main.py
@@ -78,22 +78,20 @@ def popularity_based_count(user_input=True, item_cnt=None):
     print("=" * 99)
 
 
-
-
-    # YOUR CODE GOES HERE ! -----------------------------------
-
-    query = 'SELECT item, count(user) AS count\
-            FROM ratings\
-            WHERE rating is not null\
-            GROUP BY item\
-            ORDER BY count DESC'
+    #-----------------------------------------------------------------------------------------------
+    query = 'SELECT A.item, A.count\
+            FROM (SELECT item, count(user) AS count\
+                    FROM ratings\
+                    WHERE rating is not null\
+                    GROUP BY item\
+                    ORDER BY count DESC) AS A\
+            WHERE A.item >= 150 AND A.item < 350'
     df = get_output(query)
 
     # 쿼리의 결과를 sample 변수에 저장하세요.
+    # rec_num 만큼 뽑음
     sample = df.head(rec_num)
-
-    #-----------------------------------------------------------
-
+    #-----------------------------------------------------------------------------------------------
 
 
     # do not change column names
@@ -117,25 +115,30 @@ def popularity_based_rating(user_input=True, item_cnt=None):
     print(f"Popularity Rating based recommendation")
     print("=" * 99)
 
+
     # TODO: remove sample, return actual recommendation result as df
-    # YOUR CODE GOES HERE !
-    query ='SELECT A.item, ROUND(avg(A.scaled_rating),4) AS prediction\
-            FROM (SELECT user, item, (rating-min)/(max-min) AS scaled_rating\
-                FROM ratings AS R NATURAL JOIN (SELECT user,\
-                                                CASE count(rating)\
-                                                        WHEN 1 then 0\
-                                                ELSE min(rating)\
-                                                END AS min,\
-                                                max(rating) AS max\
-                                                FROM ratings\
-                                                GROUP BY user) AS S\
-                ) AS A\
-            GROUP BY A.item\
-            ORDER BY prediction DESC'
+    #-----------------------------------------------------------------------------------------------
+    query ='SELECT F.item, F.prediction\
+            FROM (SELECT A.item, ROUND(avg(A.scaled_rating),4) AS prediction\
+                    FROM (SELECT user, item, (rating-min)/(max-min) AS scaled_rating\
+                            FROM ratings AS R NATURAL JOIN (SELECT user,\
+                                                            CASE count(rating)\
+                                                                    WHEN 1 then 0\
+                                                            ELSE min(rating)\
+                                                            END AS min,\
+                                                            max(rating) AS max\
+                                                            FROM ratings\
+                                                            GROUP BY user) AS S\
+                            ) AS A\
+                    GROUP BY A.item\
+                    ORDER BY prediction DESC) AS F\
+            WHERE F.item >= 150 AND F.item < 350'
     df = get_output(query)
 
     # 쿼리의 결과를 sample 변수에 저장하세요.
+    # rec_num 만큼 뽑음
     sample = df.head(rec_num)
+    #-----------------------------------------------------------------------------------------------
 
 
     # do not change column names
@@ -163,11 +166,66 @@ def ibcf(user_input=True, user_id=None, item_cnt=None):
     print(f'Item-based Collaborative Filtering')
     print(f'Recommendations for user {user}')
 
+
     # TODO: remove sample, return actual recommendation result as df
-    # YOUR CODE GOES HERE !
+    #-----------------------------------------------------------------------------------------------
+    # 아이템-아이템 테이블
+    query = 'SELECT item_1, sum(sim) AS sim_sum \
+            FROM (SELECT item_1, item_2, sim, ROW_NUMBER() OVER(PARTITION BY item_1 ORDER BY sim DESC, item_2 ASC) AS ranking\
+                FROM item_similarity) AS A\
+            WHERE A.ranking <= 5\
+            GROUP BY item_1'
+
+    query2 = 'SELECT I.item_1, I.item_2, ROUND(I.sim/S.sim_sum , 4) AS nor_sim\
+            FROM (SELECT item_1, item_2, sim, ROW_NUMBER() OVER(PARTITION BY item_1 ORDER BY sim DESC, item_2 ASC) AS ranking\
+                FROM item_similarity) AS I NATURAL JOIN ('+ query +') AS S\
+            WHERE I.ranking <= 5'
+
+    query3 = 'SELECT L.item_1, L.item_2,  CASE \
+                        WHEN R.nor_sim is null THEN 0\
+                        ELSE R.nor_sim\
+                        END AS chg_sim\
+            FROM item_similarity AS L LEFT OUTER JOIN (' + query2 + ') AS R USING (item_1, item_2)\
+                '
+    item_sim = get_output(query3)
+
+    # 아이템-유저 테이블 아이템 평균으로 sim 나눔
+    query = 'SELECT R.item, R.user,\
+                CASE \
+                    WHEN R.rating is null THEN T.rate_avg\
+                ELSE R.rating\
+                END AS rate\
+            FROM ratings AS R NATURAL JOIN (SELECT item, avg(rating) AS rate_avg\
+                                            FROM ratings\
+                                            GROUP BY item) AS T'
+
+    item_user = get_output(query)
+
+
+    # 아이템-아이템 피벗, 아이템-유저 피벗, 합성곱 
+    ii_pivot = pd.pivot_table(item_sim, index= 'item_1', columns= 'item_2', values= 'chg_sim')
+    iu_pivot = pd.pivot_table(item_user, index='item', columns='user', values='rate')
+    dot_df = ii_pivot.dot(iu_pivot)
+
+    # stack 형식으로 변환, 반올림
+    df_mid = dot_df.stack().reset_index()
+    df_mid[0] = df_mid[0].astype(float).round(4)
+    df_nd = df_mid.rename(columns={'item_1':'item',0:'prediction'}).sort_values(['user','prediction','item'], ascending=[True,False,True])
+    
+    # 기존에 평가했던 item 제외
+    query = 'SELECT * FROM ratings'
+    origin = get_output(query)
+    or_list = origin[(origin.user == user) & (origin.rating.isnull())].item.to_list()
+    df_nd = df_nd[df_nd.item.isin(or_list)]
+
+    # rec_num 만큼 갯수 뽑음
+    df_fin = df_nd.groupby('user').head(rec_num)[['user','item','prediction']].reset_index(drop=True)
+
     # 쿼리의 결과를 sample 변수에 저장하세요.
-    sample = [(user, 50-x, x/10)
-              for x in range(50, math.ceil(rec_num * 10) - 1, -1)]
+    # user 에 해당하는 결과 뽑음
+    sample = df_fin[df_fin.user == user]
+    #-----------------------------------------------------------------------------------------------
+
 
     # do not change column names
     df = pd.DataFrame(sample, columns=['user', 'item', 'prediction'])
@@ -196,10 +254,65 @@ def ubcf(user_input=True, user_id=None, item_cnt=None):
     print(f'Recommendations for user {user}')
 
     # TODO: remove sample, return actual recommendation result as df
-    # YOUR CODE GOES HERE !
+
+    #-----------------------------------------------------------------------------------------------------
+    # 유저-유저 테이블
+    query = 'SELECT user_1, sum(sim) AS sim_sum \
+            FROM (SELECT user_1, user_2, sim, ROW_NUMBER() OVER(PARTITION BY user_1 ORDER BY sim DESC, user_2 ASC) AS ranking\
+                FROM user_similarity) AS A\
+            WHERE A.ranking <= 5\
+            GROUP BY user_1'
+    
+    query2 = 'SELECT I.user_1, I.user_2, ROUND(I.sim/S.sim_sum , 4) AS nor_sim\
+            FROM (SELECT user_1, user_2, sim, ROW_NUMBER() OVER(PARTITION BY user_1 ORDER BY sim DESC, user_2 ASC) AS ranking\
+                FROM user_similarity) AS I NATURAL JOIN ('+ query +') AS S\
+            WHERE I.ranking <= 5'
+
+    query3 = 'SELECT L.user_1, L.user_2,  CASE \
+                        WHEN R.nor_sim is null THEN 0\
+                        ELSE R.nor_sim\
+                        END AS chg_sim\
+            FROM user_similarity AS L LEFT OUTER JOIN (' + query2 + ') AS R USING (user_1, user_2)\
+            '
+    user_sim = get_output(query3)
+
+    # 유저-아이템 테이블 유저 평균으로 sim 나눔
+    query = 'SELECT R.user, R.item,\
+                CASE \
+                    WHEN R.rating is null THEN T.rate_avg\
+                ELSE R.rating\
+                END AS rate\
+            FROM ratings AS R NATURAL JOIN (SELECT user, avg(rating) AS rate_avg\
+                                            FROM ratings\
+                                            GROUP BY user) AS T'
+    
+    user_item = get_output(query)
+
+    # 유저-유저 피벗, 유저-아이템 피벗, 합성곱 
+    uu_pivot = pd.pivot_table(user_sim, index= 'user_1', columns= 'user_2', values= 'chg_sim')
+    ui_pivot = pd.pivot_table(user_item, index='user', columns='item', values='rate')
+    dot_df = uu_pivot.dot(ui_pivot)
+
+    # stack 형식으로 변환, 반올림
+    df_mid = dot_df.stack().reset_index()
+    df_mid[0] = df_mid[0].astype(float).round(4)
+    df_nd = df_mid.rename(columns={'user_1':'user',0:'prediction'}).sort_values(['user','prediction','item'], ascending=[True,False,True])
+    
+    # 기존에 평가했던 item 제외
+    query = 'SELECT * FROM ratings'
+    origin = get_output(query)
+    or_list = origin[(origin.user == user) & (origin.rating.isnull())].item.to_list()
+    df_nd = df_nd[df_nd.item.isin(or_list)]
+
+    # rec_num 만큼 갯수 뽑음
+    df_fin = df_nd.groupby('user').head(rec_num)[['user','item','prediction']].reset_index(drop=True)
+
     # 쿼리의 결과를 sample 변수에 저장하세요.
-    sample = [(user, 50-x, x/10)
-              for x in range(50, math.ceil(rec_num * 10) - 1, -1)]
+    # user 에 해당하는 결과 뽑음
+    sample = df_fin[df_fin.user == user]
+    #-----------------------------------------------------------------------------------------------------
+
+
 
     # do not change column names
     df = pd.DataFrame(sample, columns=['user', 'item', 'prediction'])
